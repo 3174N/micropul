3e7bbbb18ee6beb8606f29a84192aceb585fc591
Ethan Levy <ethan@ethanlevy.org>
2023-02-21 21:39:56 +0200

    Add tile rotation

--- client/src/app/tile/tile.component.ts
+++ client/src/app/tile/tile.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tile.component.less'],
 })
 export class TileComponent implements OnInit {
+  rotationAngle = 0;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onClick() {
+    this.rotationAngle = (this.rotationAngle + 90) % 360;
+  }
 }
