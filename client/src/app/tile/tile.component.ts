bdfc9d1615bf2dbbca2fe7136cac41538608a2ad
Ethan Levy <ethan@ethanlevy.org>
2023-02-21 21:40:12 +0200

    Update tile image path

--- client/src/app/tile/tile.component.ts
+++ client/src/app/tile/tile.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TileComponent implements OnInit {
   rotationAngle = 0;
+  tilePath = '../../assets/tiles/tile_0.png';
 
   constructor() {}
 
