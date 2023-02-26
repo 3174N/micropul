4d61bcb560cd49d8c224fc35cf0e7d8d9c2be2a0
Ethan Levy <ethan@ethanlevy.org>
2023-02-26 21:02:31 +0200

    Update grid

--- client/src/app/grid/grid.component.ts
+++ client/src/app/grid/grid.component.ts
@@ -1,7 +1,4 @@
-import {
-  CdkDragDrop,
-  moveItemInArray,
-} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,13 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridComponent implements OnInit {
   tiles = [
-    ['0', '1'],
-    ['2', '3'],
+    ['', '', ''],
+    ['', '', '3'],
+    ['2', '1', '0'],
+    ['', '13', ''],
+    ['', '', ''],
   ];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let newTiles = [];
+    for (let i = 0; i < this.tiles.length; i++) {
+      let row = [];
+      for (let j = 0; j < this.tiles[i].length; j++) {
+        row.push(
+          this.tiles[i][j] != ''
+            ? this.tiles[i][j]
+            : (i < this.tiles.length - 1
+                ? this.tiles[i + 1][j] != ''
+                : false) ||
+              (j < this.tiles[i].length - 1
+                ? this.tiles[i][j + 1] != ''
+                : false) ||
+              (j > 0 ? this.tiles[i][j - 1] != '' : false) ||
+              (i > 0 ? this.tiles[i - 1][j] != '' : false)
+            ? ''
+            : '-1'
+        );
+      }
+      newTiles.push(row);
+    }
+    this.tiles = newTiles;
+    console.log(this.tiles);
+  }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
