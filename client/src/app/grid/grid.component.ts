12201862b25dbf3396a1fbeec82fa8738f186e12
Ethan Levy <ethan@ethanlevy.org>
2023-04-25 13:05:45 +0300

    Add tile struct

--- client/src/app/grid/grid.component.ts
+++ client/src/app/grid/grid.component.ts
@@ -1,6 +1,11 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
+interface Tile {
+  state: number; // -1: Empty tile; 0: Placeable tile; 1: Occupied tile.
+  tileIndex: string | null;
+}
+
 @Component({
   selector: 'tile-grid',
   templateUrl: './grid.component.html',
@@ -10,14 +15,14 @@ export class GridComponent implements OnInit {
   indexOver: number = -1;
   previewStyle: any = null;
 
-  tiles: string[][] = new Array(48 * 2 - 1).fill(
-    new Array(48 * 2 - 1).fill(' ')
+  tiles: Tile[][] = new Array(48 * 2 - 1).fill(
+    new Array(48 * 2 - 1).fill({ state: -1, tileIndex: null })
   );
 
   constructor() {}
 
   ngOnInit(): void {
-    this.tiles[48][48] = '1';
+    this.tiles[48][48] = { state: 1, tileIndex: '0' };
 
     this.updateGrid();
   }
@@ -28,18 +33,18 @@ export class GridComponent implements OnInit {
       let row = [];
       for (let j = 0; j < 48 * 2 - 1; j++) {
         row.push(
-          this.tiles[i][j] != ' '
+          this.tiles[i][j].state != -1
             ? this.tiles[i][j]
             : (i < this.tiles.length - 1
-                ? this.tiles[i + 1][j] != ' '
+                ? this.tiles[i + 1][j].state == 1
                 : false) ||
               (j < this.tiles[i].length - 1
-                ? this.tiles[i][j + 1] != ' '
+                ? this.tiles[i][j + 1].state == 1
                 : false) ||
-              (j > 0 ? this.tiles[i][j - 1] != ' ' : false) ||
-              (i > 0 ? this.tiles[i - 1][j] != ' ' : false)
-            ? ' '
-            : '-1'
+              (j > 0 ? this.tiles[i][j - 1].state == 1 : false) ||
+              (i > 0 ? this.tiles[i - 1][j].state == 1 : false)
+            ? { state: 0, tileIndex: null }
+            : { state: -1, tileIndex: null }
         );
       }
       newTiles.push(row);
@@ -52,9 +57,12 @@ export class GridComponent implements OnInit {
     // previous container = dragged tile.
     // current container = tile in dropped position.
 
-    if (event.container.data != ' ') return;
+    if (event.container.data.state != 0) return;
 
-    this.tiles[row][col] = event.previousContainer.data;
+    this.tiles[row][col] = {
+      state: 1,
+      tileIndex: event.previousContainer.data,
+    };
     this.updateGrid();
   }
 }
