a41e08ede3c7e6ff113dc6e2c2c973f78516cbc4
Ethan Levy <ethan@ethanlevy.org>
2023-04-28 17:32:48 +0300

    Add grid rendering

--- client/src/app/grid/grid.component.ts
+++ client/src/app/grid/grid.component.ts
@@ -1,8 +1,14 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, HostListener, Input, OnInit } from '@angular/core';
 
+interface Coords {
+  x: number;
+  y: number;
+}
+
 interface Tile {
-  state: number; // -1: Empty tile; 0: Placeable tile; 1: Occupied tile.
+  position: Coords;
+  isTile: boolean; // Non-tile are places near tiles that can be placed on.
   tileIndex: string | null;
 }
 
@@ -15,9 +21,7 @@ export class GridComponent implements OnInit {
   indexOver: number = -1;
   previewStyle: any = null;
 
-  tiles: Tile[][] = new Array(48 * 2 - 1).fill(
-    new Array(48 * 2 - 1).fill({ state: -1, tileIndex: null })
-  );
+  tiles: Tile[] = [{ position: { x: 3, y: 2 }, isTile: true, tileIndex: '0' }];
 
   readonly SCROLL_THRESH = 50;
   readonly MIN_SCALE = 0.25;
@@ -36,48 +40,47 @@ export class GridComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.tiles[48][48] = { state: 1, tileIndex: '0' };
+    this.tiles.push({ position: { x: 0, y: 0 }, isTile: true, tileIndex: '4' });
+    this.tiles.push({ position: { x: 2, y: 1 }, isTile: true, tileIndex: '7' });
+    this.tiles.push({
+      position: { x: 3, y: 3 },
+      isTile: true,
+      tileIndex: '14',
+    });
+    this.tiles.push({
+      position: { x: 7, y: 2 },
+      isTile: true,
+      tileIndex: '28',
+    });
+    this.tiles.push({ position: { x: 0, y: 1 }, isTile: true, tileIndex: '3' });
 
     this.updateGrid();
   }
 
   updateGrid() {
-    let newTiles = [];
-    for (let i = 0; i < 48 * 2 - 1; i++) {
-      let row = [];
-      for (let j = 0; j < 48 * 2 - 1; j++) {
-        row.push(
-          this.tiles[i][j].state != -1
-            ? this.tiles[i][j]
-            : (i < this.tiles.length - 1
-                ? this.tiles[i + 1][j].state == 1
-                : false) ||
-              (j < this.tiles[i].length - 1
-                ? this.tiles[i][j + 1].state == 1
-                : false) ||
-              (j > 0 ? this.tiles[i][j - 1].state == 1 : false) ||
-              (i > 0 ? this.tiles[i - 1][j].state == 1 : false)
-            ? { state: 0, tileIndex: null }
-            : { state: -1, tileIndex: null }
-        );
-      }
-      newTiles.push(row);
-    }
-    this.tiles = newTiles;
-    console.log(this.tiles);
-  }
-
-  drop(event: CdkDragDrop<any>, row: number, col: number) {
-    // previous container = dragged tile.
-    // current container = tile in dropped position.
-
-    if (event.container.data.state != 0) return;
-
-    this.tiles[row][col] = {
-      state: 1,
-      tileIndex: event.previousContainer.data,
-    };
-    this.updateGrid();
+    // let newTiles = [];
+    // for (let i = 0; i < 48 * 2 - 1; i++) {
+    //   let row = [];
+    //   for (let j = 0; j < 48 * 2 - 1; j++) {
+    //     row.push(
+    //       this.tiles[i][j].state != -1
+    //         ? this.tiles[i][j]
+    //         : (i < this.tiles.length - 1
+    //             ? this.tiles[i + 1][j].state == 1
+    //             : false) ||
+    //           (j < this.tiles[i].length - 1
+    //             ? this.tiles[i][j + 1].state == 1
+    //             : false) ||
+    //           (j > 0 ? this.tiles[i][j - 1].state == 1 : false) ||
+    //           (i > 0 ? this.tiles[i - 1][j].state == 1 : false)
+    //         ? { state: 0, tileIndex: null }
+    //         : { state: -1, tileIndex: null }
+    //     );
+    //   }
+    //   newTiles.push(row);
+    // }
+    // this.tiles = newTiles;
+    // console.log(this.tiles);
   }
 
   clamp = (value: number, min: number, max: number) =>
