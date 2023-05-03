6587aa2ee4b59ae362136080a16860a4cba5378c
Ethan Levy <ethan@ethanlevy.org>
2023-05-03 19:59:33 +0300

    Add supply->hand

--- client/src/app/supply/supply.component.ts
+++ client/src/app/supply/supply.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'supply',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./supply.component.less'],
 })
 export class SupplyComponent {
-  supply: string[] = ['1'];
+  supply: string[] = ['1', '2', '3'];
+
+  constructor(private sharedService: SharedService) {}
+
+  click() {
+    if (this.supply.length <= 0) return;
+
+    // Remove tile from supply and add it to hand.
+    let tile = this.supply[0];
+    this.supply.splice(0, 1);
+    let hand = this.sharedService.getHand();
+    hand.push(tile);
+    this.sharedService.setHand(hand);
+  }
+
+  addTile(tile: string) {
+    this.supply.push(tile);
+  }
 }
