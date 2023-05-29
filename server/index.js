526ce65d87a1db9fa0e26de6b16b6a9969126649
Ethan Levy <ethan@ethanlevy.org>
2023-05-29 16:03:10 +0300

    Add test server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,31 @@
-const express = require('express');
-const http = require('http');
-const socketio = require('socketio');
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketIO(server);
 
-app.use(express.static('public'));
+const port = 3000; // Replace with your desired port number
+
+io.on("connection", (socket) => {
+  console.log("A client has connected");
+
+  // Event listener for receiving messages from the client
+  socket.on("message", (data) => {
+    console.log("Received message:", data);
+
+    // Echo the message back to the client
+    socket.emit("message", data);
+  });
+
+  // Event listener for when the client disconnects
+  socket.on("disconnect", () => {
+    console.log("A client has disconnected");
+  });
+});
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
